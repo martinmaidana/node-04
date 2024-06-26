e78d8c1645f3e1e7c1eb79910aeb512476dd611d
Martin Maidana <martinnicolasmaidana@gmail.com>
2024-06-26 17:42:02 -0300

    node04-rutas

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 
 app.use(express.static("public"));
 
+//con este middleware me traigo el post que creé
+app.use(express.json());
+// const productosRouter = require('./routes/productos.router')
+// app.use('/productos',productosRouter);
+app.use("/productos", require("./routes/productos.router"));
+
 // http://localhost:3000/
 app.get("/", (req, res) => {
   res.send("Hola Express!");
